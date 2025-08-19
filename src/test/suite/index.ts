5aa3267aead50feed71697b8485b4d3c0b2e2466
Wan <vuquangkhtn@gmail.com>
2025-08-19 13:41:10 +0700

    resolve CI failed

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import Mocha from 'mocha';
-import glob from 'glob';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
     // Create the mocha test
@@ -12,17 +12,13 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((resolve, reject) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err: any, files: any) => {
-            if (err) {
-                return reject(err);
-            }
-
+        glob('**/**.test.js', { cwd: testsRoot }).then((files: string[]) => {
             // Add files to the test suite
-            files.forEach((f: any) => mocha.addFile(path.resolve(testsRoot, f)));
+            files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
             try {
                 // Run the mocha test
-                mocha.run((failures: any) => {
+                mocha.run((failures: number) => {
                     if (failures > 0) {
                         reject(new Error(`${failures} tests failed.`));
                     } else {
@@ -30,8 +26,11 @@ export function run(): Promise<void> {
                     }
                 });
             } catch (err) {
+                console.error(err);
                 reject(err);
             }
+        }).catch((err: any) => {
+            return reject(err);
         });
     });
 }
